f3f29e04678eed3da666b17f58f22c41344a1503
Boburbek6010 <boburbekkhusanboev@gmail.com>
2023-12-04 12:42:21 +0500

    truth

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
-
 import 'package:http/http.dart';
 import 'package:testing_console_apps/menus/home_menu.dart';
 import 'package:testing_console_apps/menus/setting_menu.dart';
+import 'package:testing_console_apps/models/lang_model.dart';
 import 'package:testing_console_apps/my_app.dart';
 import 'package:testing_console_apps/services/extension_service.dart';
 import 'package:testing_console_apps/services/io_service.dart';
@@ -12,7 +12,7 @@ import 'package:translator/translator.dart';
 Future<void> main() async {
 
   // while(true){
-  //
+
   //   print("welcome".tr);
   //
   //   print("I uz");
@@ -25,28 +25,58 @@ Future<void> main() async {
   //
   //   print("welcome".tr);
   // }
+  //
+  // var translate = GoogleTranslator();
+  //
+  //
+  // translate.translateAndPrint("text", to: "uz");
 
 
 
-  var translator = GoogleTranslator();
+  // checkLang();
 
+
+
+
+
+
+
+
+
+  MyApp(
+      home: HomeMenu(),
+      lang: Language.uz,
+      routes: {
+        HomeMenu.id:HomeMenu(),
+        SettingMenu.id:SettingMenu()
+      },
+  );
+}
+
+
+Future<void> checkLang() async {
+  print("Enter text");
   String str = IOService.read();
+  print("from");
+  String from = IOService.read();
+  print("to");
+  String to = IOService.read();
 
-  Translation result = await str.translate(to: "uz", from: "");
+  bool isFrom = LanguageList.langs.containsKey(from);
+  bool isTo = LanguageList.langs.containsKey(to);
 
-  IOService.write(result);
+  if(isFrom && isTo) {
+    Translation result = await str.translate(to: to, from: from);
+    IOService.write(result);
+  }else{
+    print("Tanlangan til mavjud emas\n");
 
+    print("Mavjud tillar: ");
+    LanguageList.langs.forEach((key, value) {
+      print("Key: $key : Value: $value");
+    });
 
-
-
-
-
-  // MyApp(
-  //     home: HomeMenu(),
-  //     lang: Language.uz,
-  //     routes: {
-  //       HomeMenu.id:HomeMenu(),
-  //       SettingMenu.id:SettingMenu()
-  //     },
-  // );
+    print("Qaytadan urining");
+    checkLang();
+  }
 }
